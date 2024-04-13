d49e2c3f003cfc79dd9cbe74e661c2216c1e0d92
Cole <coltonflather@gmail.com>
2024-04-13 18:24:38 -0400

    updateMainJSFileForGoogleAuth

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+function openGoogleLoginPopup() {
+  // Replace these with your actual client ID and redirect URI
+  var clientId = '639047990456-94arvdhfleq94ereo1jq32g5u0sjqjjm.apps.googleusercontent.com';
+  var redirectUri = 'https://cijibin314.github.io';
 
-console.log("hi")
-const coleId = "549870321204-1pe8tqh3drr0mcft6room7rln2tr7bk6.apps.googleusercontent.com"
-/*window.onload = function () {
-    /*google.accounts.id.initialize({
-      client_id: coleId,
-      callback: handleCredentialResponse
-    });
-    google.accounts.id.prompt();
-  };
+  // Construct the Google OAuth URL
+  var googleOAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth?' +
+      'client_id=' + encodeURIComponent(clientId) +
+      '&redirect_uri=' + encodeURIComponent(redirectUri) +
+      '&response_type=code' +
+      '&scope=email profile openid';
 
-function handleCredentialResponse(authResult) {
-    console.log(authResult);
-};*/
+  // Open the Google OAuth URL in a new window
+  var popup = window.open(googleOAuthUrl, '_blank', 'width=600,height=600');
+  if (!popup) {
+      alert('Please allow popups for this website');
+  }
+}
